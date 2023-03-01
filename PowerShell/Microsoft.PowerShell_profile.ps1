9dd075628af1c533c158065e4c6be0be7692c0a0
Miguel Alves <miguelalves1258@gmail.com>
2023-03-01 12:52:00 -0300

    Ambiente de Trabalho

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -6,11 +6,32 @@ Invoke-Expression (&starship init powershell)
 $ENV:STARSHIP_CONFIG = "C:\Users\miguel\.config\starship.toml"
 
 function dev {
-    param ($location)
-    Switch ($location) {
+    param([string]$local)
+    Switch ($local) {
         "macb"   { Set-Location -Path "$HOME/Projetos/MacB/" }
         "miguel" { Set-Location -Path "$HOME/Projetos/Miguel" }
         "mirage" { Set-Location -Path "$HOME/Projetos/Mirage" }
         default  { Set-Location -Path "$HOME/Projetos/" }
+    }
+    Get-ChildItem
+}
+
+function novo {
+    param([string]$nome, [string]$conteudo)
+
+    try {
+        New-Item -ItemType File $nome
+        Set-Content -Path $nome -Value $conteudo
+    }
+    catch {
+        Write-Host "NOVO ARQUIVO"
+        Write-Host ""
+        Write-Host "Uso: novo <nome_arquivo> <conteudo>"
+        Write-Host "     novo -nome '<nome_arquivo>' -conteudo '<conteudo>'"
+        Write-Host ""
+        Write-Host "-nome           O nome do arquivo junto com sua extenção."
+        Write-Host "-conteudo       O conteúdo do arquivo."
+        Write-Host ""
+        Write-Host "Exemplo: novo -nome 'nome do arquivo.txt' -conteudo 'Olá Mundo'"
     }
 }
